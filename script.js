f07ca2935406a0e6faece7702a7c1895c02f60ce
Carbonarik <129664493+Carbonarik@users.noreply.github.com>
2023-04-02 17:10:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("telegram-button").addEventListener("click", function() {
+  window.location.href = "https://t.me/Karalineverny";
+});
+
+document.getElementById("steam-button").addEventListener("click", function() {
+  window.location.href = "https://steamcommunity.com/profiles/76561199390685814/";
+});
+
+document.getElementById("vk-button").addEventListener("click", function() {
+  window.location.href = "https://vk.com/carbonarik";
+});
